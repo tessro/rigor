a67b96037418858dc49fbe356b88fa27a9ff500e
Paul Rosania <paul.rosania@gmail.com>
2013-01-21 23:12:45 -0800

    Mean, proportion, variance and z values

--- lib/rigor/treatment.rb
+++ lib/rigor/treatment.rb
@@ -36,5 +36,30 @@ module Rigor
     def record_event!(event, object)
       Rigor.connection.record_event!(self, object, event)
     end
+
+    def variance(event)
+      dist = event_distribution(event)
+      m = mean(event)
+      dist.each_with_index.reduce(0.0) { |sse, d| sse + d[0] * (d[1] - m) ** 2 } / size
+    end
+
+    def mean(event)
+      total_events(event).to_f / size
+    end
+
+    def proportion(event)
+      unique_events(event).to_f / size
+    end
+
+    def z_value(event, control)
+      se_diff = Math.sqrt(variance(event) / size + control.variance(event) / control.size)
+      (mean(event) - control.mean(event)) / se_diff
+    end
+
+    def unique_z_value(event, control)
+      pooled_p = (unique_events(event) + control.unique_events(event)).to_f / (size + control.size)
+      diff_p = proportion(event) - control.proportion(event)
+      diff_p / Math.sqrt(pooled_p * (1 - pooled_p) * (1 / size.to_f + 1 / control.size.to_f))
+    end
   end
 end
